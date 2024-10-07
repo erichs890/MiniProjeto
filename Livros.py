5bc484bdbeaee19e9de6a10f75fa1f808360f164
Erich Schlaepfer <114402221+erichs890@users.noreply.github.com>
2024-10-07 12:18:02 -0300

    Add files via upload

---
+++ Livros.py
@@ -0,0 +1,8 @@
+class livro:
+    def __init__(self, id:int, titulo:str, autor:str, ano:int,  status:str):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.status = status
+        
