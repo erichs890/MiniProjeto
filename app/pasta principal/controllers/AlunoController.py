b7e757509137b70ade0719aee69ac527c4e4834e
erichs890 <erichs890@gmail.com>
2024-10-21 19:54:33 -0300

    finalizado aluno

--- app/pasta principal/controllers/AlunoController.py
+++ app/pasta principal/controllers/AlunoController.py
@@ -1,19 +1,26 @@
 from serviços.AlunoServico import AlunoServico
-from alunosFiltro import AlunosFiltro
 from views.AlunoView import AlunoView
 
 class AlunoController:
     def __init__(self, url):
         self.aluno_servico = AlunoServico(url)
-        self.filtro = AlunosFiltro()
         self.view = AlunoView()
 
+    # Função de filtragem movida para o controller
+    def filtrar_alunos_historia_presencial(self, dados):
+        alunos_historia = []
+        for aluno in dados:
+            if aluno['curso'] == 'História' and aluno['modalidade'] == "Presencial":
+                alunos_historia.append(aluno['nome'])
+        return alunos_historia
+
+    # Função principal para listar alunos de História
     def listar_alunos_historia(self):
         # Obtém os alunos do serviço
         dados = self.aluno_servico.getAlunos()
-        
-        # Aplica o filtro para alunos de História na modalidade Presencial
-        alunos_historia = self.filtro.filtroHistoria(dados)
-        
+
+        # Filtra os alunos de História presencial
+        alunos_historia = self.filtrar_alunos_historia_presencial(dados)
+
         # Passa os alunos filtrados para a view
         self.view.listar_alunos(alunos_historia)
--- app/pasta principal/main.py
+++ app/pasta principal/main.py
@@ -1,10 +1,7 @@
 from controllers.AlunoController import AlunoController
 
-# URL do microsserviço
 url = "https://rmi6vdpsq8.execute-api.us-east-2.amazonaws.com/msAluno"
 
-# Instanciando o controlador de alunos
 aluno_controller = AlunoController(url)
 
-# Chamando a função que lista alunos de História na modalidade presencial
 aluno_controller.listar_alunos_historia()
