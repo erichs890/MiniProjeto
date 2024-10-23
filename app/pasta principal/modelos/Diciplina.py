68b55417603fd8fdfa4b3a81e1ca436002992b00
haaphak <carlosdirceu519@gmail.com>
2024-10-23 12:07:26 -0300

    modelos do livro e das diciplinas

--- app/pasta principal/modelos/Diciplina.py
+++ app/pasta principal/modelos/Diciplina.py
@@ -1,6 +1,13 @@
-class diciplina:
-    def __init__(self, id:int, curso:str, nome:str):
-        self.id = id
-        self.curso = curso
-        self.nome = nome
-        
+import requests
+
+class DiciplinaModelo:
+    def __init__(self, urlBase):
+        self.urlBase = urlBase
+
+    def getDiciplina(self):
+        response = requests.get(self.urlBase)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Erro ao buscar dados")
+            return []
--- app/pasta principal/modelos/Livros.py
+++ app/pasta principal/modelos/Livros.py
@@ -1,8 +1,13 @@
-class livro:
-    def __init__(self, id:int, titulo:str, autor:str, ano:int,  status:str):
-        self.id = id
-        self.titulo = titulo
-        self.autor = autor
-        self.ano = ano
-        self.status = status
-        
+import requests
+
+class LivroModelo:
+    def __init__(self, urlBase):
+        self.urlBase = urlBase
+
+    def getLivro(self):
+        response = requests.get(self.urlBase)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Erro ao buscar dados")
+            return []
